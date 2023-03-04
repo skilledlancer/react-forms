5ae0338a51b760f4b91c4e7933cb8edaee9da178
Manish Kumar Namdeo <mknamdeo@gmail.com>
2023-03-04 17:01:02 +0530

    6.123: Input state and submit handler setup

---
+++ src/components/controlled-inputs.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const ControlledInputs = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email)
+  }
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h4>Controlled Inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="form-input"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="form-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button type="submit" className="btn btn-block">
+        submit
+      </button>
+    </form>
+  )
+}
+export default ControlledInputs
